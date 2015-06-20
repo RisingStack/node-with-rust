4dae2cf15089428b5131b9af2afa3aa495722af7
Gergely Nemeth <mail@nemethgergely.com>
2015-06-20 20:36:51 +0200

    Adding fibonacci

--- src/embed.rs
+++ src/embed.rs
@@ -3,3 +3,12 @@
 pub extern fn process(a: i32, b: i32) -> i32 {
     return a * b;
 }
+
+#[no_mangle]
+pub extern fn fibonacci(x: i32) -> i32 {
+  if x <= 2 {
+    return 1;
+  } else {
+    return fibonacci(x - 1) + fibonacci(x - 2);
+  }
+}
